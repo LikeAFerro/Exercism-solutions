94e49bd9713adc63a9b88f2bc1f1de066000408e
LikeAFerro <ferrarifra97@gmail.com>
2026-04-22 10:48:19 +0200

    Exercise 3 done

---
+++ c/difference-of-squares/difference_of_squares.h
@@ -0,0 +1,8 @@
+#ifndef DIFFERENCE_OF_SQUARES_H
+#define DIFFERENCE_OF_SQUARES_H
+
+unsigned long long sum_of_squares(unsigned int number);
+unsigned long long square_of_sum(unsigned int number);
+unsigned long long difference_of_squares(unsigned int number);
+
+#endif
