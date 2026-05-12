174dbba36e94747d31166eb735ce8450630628c2
LikeAFerro <ferrarifra97@gmail.com>
2026-05-12 11:29:50 +0200

    Fixed pangram style

--- c/pangram/pangram.c
+++ c/pangram/pangram.c
@@ -1,13 +1,13 @@
 #include "pangram.h"
 #include <ctype.h>
 #include <stdint.h>
-#include <string.h>
 
 bool is_pangram(const char *sentence) {
     if (!sentence)
         return false;
     int32_t alphabet = 0;
-    for (size_t i = 0; sentence[i] != '\0'; i++) {
+    size_t i = 0;
+    while (sentence[i] != '\0') {
         if (isalpha((unsigned char) sentence[i])) {
             int index = tolower((unsigned char) sentence[i]) - 'a';
             // every bit represents a letter, Z to A (left to right). 3FFFFF is 26 1s (binary) in
@@ -16,6 +16,7 @@ bool is_pangram(const char *sentence) {
             if (alphabet == 0x3FFFFFF)
                 return true;
         }
+        i++;
     }
     return false;
 }
